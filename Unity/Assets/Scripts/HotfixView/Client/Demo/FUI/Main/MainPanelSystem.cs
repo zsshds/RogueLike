dba3f64dd8b95fc8ee8b6675ee9e6a711cb64208
刘沟沟 <12197168+liugougou@user.noreply.gitee.com>
2025-11-25 22:59:52 +0800

    添加了一些事件的绑定，有bug需要解决

--- Unity/Assets/Scripts/HotfixView/Client/Demo/FUI/Main/MainPanelSystem.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/FUI/Main/MainPanelSystem.cs
@@ -27,12 +27,42 @@ namespace ET.Client
             FUI_JoystickComponent joystickComponent = self.FUIMainPanel.JoystickComponent;
             Joystick joystickMono = joystickComponent.displayObject.gameObject.AddComponent<Joystick>();
             joystickMono.Init(joystickComponent.Btn_Joystick, joystickComponent.Img_JoystickBG);
+            //向mono中添加事件监听，其实也可以使用Action委托实现
+            joystickMono.OnJoystickTouchEnd.AddListener(self.JoystickTouchEnd);
+            joystickMono.OnJoystickTouchMove.AddListener(self.JoystickTouchMove);
         }
         
         //摇杆事件
         private static void JoystickTouchEnd(this MainPanel self, Vector2 v)
         {
-            self.Scene().GetComponent<OperaComponent>().StopMove();
+            OperaComponent operaComponent = self.Root().Scene().GetComponent<OperaComponent>();
+            if (operaComponent != null)
+            {
+                operaComponent.StopMove();
+            }
+            else
+            {
+                Log.Error($"OperateInfos is null");
+            }
+            
+            //self.Scene().GetComponent<OperaComponent>().StopMove();
+        }
+        
+        private static void JoystickTouchMove(this MainPanel self, Vector2 v)
+        { 
+            if (v == Vector2.zero)
+                return;
+            OperaComponent operaComponent = self.Root().Scene().GetComponent<OperaComponent>();
+            if (operaComponent != null)
+            {
+                operaComponent.OnMove(v);
+            }
+            else
+            {
+                Log.Warning($"当前Scene 为 {self.Scene().Name}， root为 {self.Root().Name}");
+                Log.Error($"OperateInfos is null");
+            }
+            //self.Scene().GetComponent<OperaComponent>().OnMove(v);
         }
         #endregion
         
--- Unity/Assets/Scripts/ModelView/Client/Demo/Opera/OperaComponent.cs
+++ Unity/Assets/Scripts/ModelView/Client/Demo/Opera/OperaComponent.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ET.Client
 {
 	[ComponentOf(typeof(Scene))]
-	public class OperaComponent: Entity, IAwake, IUpdate
+	public class OperaComponent: Entity, IAwake, IUpdate, ILateUpdate
     {
         public Vector3 ClickPoint;
 
 	    public int mapMask;
+	    
+	    //操作信息
+	    public List<OperateInfo> OperateInfos = new List<OperateInfo>();
+	    public List<OperateInfo> OperateInfosTemp = new List<OperateInfo>();
     }
 }
